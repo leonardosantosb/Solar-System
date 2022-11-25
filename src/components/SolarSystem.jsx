844e54dfb208b30985a32b7c4c887e351ed4929f
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-25 20:51:21 -0300

    Requisitos 2, 3 e 4 - refatorando

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Title from './components/Title';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline='Planetas' />
+        <Title headline="Planetas" />
       </div>
     );
   }
